54db377e1e482762ace5a080e71cb7a4ca694c58
Heeme Beeme <heemebeeme@gmail.com>
2025-10-15 12:18:29 -0300

    Chromatic aberration and film grain settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -8,6 +8,9 @@ public class SettingsMenu : MonoBehaviour
     public Volume Volume;
     VolumeProfile Profile;
 
+    public UnityEngine.UI.Toggle ChromaticToggle;
+    public UnityEngine.UI.Toggle GrainToggle;
+
     public float Brightness = 1;
 
     void Start()
@@ -17,6 +20,8 @@ public class SettingsMenu : MonoBehaviour
 
     void Update()
     {
+        #region Brightness
+
         Brightness = BrightnessSlider.value;
 
         if(!Profile.TryGet<ColorAdjustments>(out var colorAdj))
@@ -25,6 +30,34 @@ public class SettingsMenu : MonoBehaviour
         }
 
         colorAdj.postExposure.value = Brightness;
+        #endregion
 
+        #region Chromatic Aberration
+
+        if (!Profile.TryGet<ChromaticAberration>(out var chromaticAbr))
+        {
+            chromaticAbr = Profile.Add<ChromaticAberration>(false);
+        }
+
+        chromaticAbr.active = ChromaticToggle.isOn;
+
+        #endregion
+
+        #region Film Grain
+
+        if (!Profile.TryGet<FilmGrain>(out var filmGrain))
+        {
+            filmGrain = Profile.Add<FilmGrain>(false);
+        }
+
+        filmGrain.active = GrainToggle.isOn;
+
+        #endregion
+
+    }
+
+    public void ResetBrightness()
+    {
+        BrightnessSlider.value = 1;
     }
 }
